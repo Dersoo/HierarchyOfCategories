78a2fe699746c65be84506d2c94a452ae7d7a69f
Dersoo <deroo@dersoo>
2023-06-16 18:25:53 +0300

    Add "Categories" controller with injected "Category" service and with Get method only

---
+++ CategoriesWebApi/CategoriesWebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,25 @@
+﻿using CategoriesWebApi.Models;
+using CategoriesWebApi.Services.CategoryService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CategoriesWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            this._categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Category>> Get()
+        {
+            return _categoryService.GetJsonTree();
+        }
+    }
+}
