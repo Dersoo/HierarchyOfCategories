c458e307c7fa72240adfbbb624954ee87b881206
Dersoo <deroo@dersoo>
2023-06-16 18:42:46 +0300

    Add "Tree" component, add route to this component.

--- categories/src/app/app-routing.module.ts
+++ categories/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TreeComponent } from './tree/tree.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'tree', component: TreeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- categories/src/app/app.module.ts
+++ categories/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TreeComponent } from './tree/tree.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TreeComponent
   ],
   imports: [
     BrowserModule,
