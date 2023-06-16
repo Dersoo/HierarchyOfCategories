633adc0deaa1bcf1c0c14a75d0b7b0c3fb1dd05a
Dersoo <deroo@dersoo>
2023-06-16 17:54:18 +0300

    Add "Category" model

---
+++ CategoriesWebApi/CategoriesWebApi/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace CategoriesWebApi.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; } = String.Empty;
+        public int? ParentCategoryId { get; set; }
+        public List<Category> ChildCategories { get; set; } = new List<Category>();
+    }
+}
